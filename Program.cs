d9f6a7bc4a308eb33b4c90495f4ad6decc6faa39
HTET Swam HTET <105802311@students.swinburne.edu.my>
2025-09-01 22:45:18 +0800

    proj initialized

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CounterProgram{
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("Hello World");
+        }
+
+        static void PrintCounters(Counter[] counters){
+            int index = 0;
+            foreach(Counter c in counters){
+                Console.WriteLine($"{index} is {c.Value}");
+                index++;
+            }
+        }
+    }
+}
